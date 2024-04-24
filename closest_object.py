ab8741454e81be04969756705c1328f719844458
Juan Pablo Lorenzo <juanisuy@gmail.com>
2024-04-24 10:26:17 -0300

    Agrego topic para la direccion en la que rotar

--- closest_object.py
+++ closest_object.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
+import numpy as np
 
 def callback(scan):
     min_distance = float('inf')
@@ -13,16 +14,23 @@ def callback(scan):
             min_index = i
 
     angle_of_closest_object = scan.angle_min + min_index * scan.angle_increment
+    direction_clocwise = False
+    
+    if (angle_of_closest_object > np.pi):
+        angle_of_closest_object = 2 * np.pi - angle_of_closest_object
+        direction_clocwise = True
     
     angle_pub.publish(angle_of_closest_object)
+    direction_clockwise.publish(direction_clocwise)
 
     # Log the closest distance and its angle
-    print(f"Distance: {min_distance: .2f} meters, Angle: {angle_of_closest_object: .2f} radians")
+    print(f"Distance: {min_distance: .2f} meters, Angle: {angle_of_closest_object: .2f} radians, Clockwise: {clockwise}")
 
 def listener():
     rospy.init_node('closest_object')
     global angle_pub
     angle_pub = rospy.Publisher('rotation_angle', Float64, queue_size=10)
+    direction_pub = rospy.Publisher('direction_clockwise', Bool, queue_size=10)
     rospy.Subscriber("/scan", LaserScan, callback)
     rospy.spin()
 
