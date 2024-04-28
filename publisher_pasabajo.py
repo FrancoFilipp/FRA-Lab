40ae27c7e33822e9afd0cb215122904620cd5757
RLopez112 <rodrigo.f.lopez.v@gmail.com>
2024-04-28 10:35:42 -0300

    nodo que publica pasabajo del sensado del tópico ADC

---
+++ publisher_pasabajo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import rospy
+from rosserial_arduino.msg import Adc
+
+#asigno filtro pasabajo
+pasabajo_alpha = 0.5
+
+# funcion de cálculo psabajo
+def pasabajo(alpha,sensor_read):
+    out_old = 0
+    out_new = out_old + alpha*(sensor_read-out_old)
+    out_old = out_new
+    return out_new
+
+#funcion callback se subscriber
+def callback_publish_pasabajo(msg:Adc):
+
+    
+    valor_pasabajo = pasabajo(pasabajo_alpha,msg.adc0)
+    
+    msg.adc0 = int(valor_pasabajo)
+    msg.adc1 = 0
+    msg.adc2 = 0
+    msg.adc3 = 0
+    msg.adc4 = 0
+    msg.adc5 = 0
+
+    rospy.loginfo("valor crudo " + str(msg.adc0))
+    rospy.loginfo("valor pasabajo " + str(valor_pasabajo) )
+    
+    pub.publish(msg)
+
+
+if __name__ == '__main__':
+    rospy.init_node("filtro_pasabajo")
+    rospy.loginfo("node started")
+    pub = rospy.Publisher("/my_topic/adc_filtrado",Adc,queue_size=10)
+    sub = rospy.Subscriber("/adc", Adc, callback = callback_publish_pasabajo)
+
+    rospy.loginfo("node started")
+
+    rospy.spin()
